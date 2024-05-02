912700238cddbe5a8bcdc61404c828c4e88345e5
embryzon <95128949+embryzon@users.noreply.github.com>
2024-05-03 02:20:37 +0800

    add answers

--- problemSet.js
+++ problemSet.js
@@ -1,28 +1,32 @@
 export const LGIN1ProblemSet = [
     {
+        // ANSWER:  A[A
         axiom: 'A',
         pre: 'A',
         Y: 'A[A[A[A'
     },
     {
+        // ANSWER: F[+F]F[−F]F
         axiom: 'F',
         pre: 'F',
         Y: 'F[+F]F[−F]F[+F[+F]F[−F]F]F[+F]F[−F]F[−F[+F]F[−F]F]F[+F]F[−F]F[+F[+F]F[−F]F[+F[+F]F[−F]F]F[+F]F[−F]F[−F[+F]F[−F]F]F[+F]F[−F]F]F[+F]F[−F]F[+F[+F]F[−F]F]F[+F]F[−F]F[−F[+F]F[−F]F]F[+F]F[−F]F[−F[+F]F[−F]F[+F[+F]F[−F]F]F[+F]F[−F]F[−F[+F]F[−F]F]F[+F]F[−F]F]F[+F]F[−F]F[+F[+F]F[−F]F]F[+F]F[−F]F[−F[+F]F[−F]F]F[+F]F[−F]F[+F[+F]F[−F]F[+F[+F]F[−F]F]F[+F]F[−F]F[−F[+F]F[−F]F]F[+F]F[−F]F[+F[+F]F[−F]F[+F[+F]F[−F]F]F[+F]F[−F]F[−F[+F]F[−F]F]F[+F]F[−F]F]F[+F]F[−F]F[+F[+F]F[−F]F]F[+F]F[−F]F[−F[+F]F[−F]F]F[+F]F[−F]F[−F[+F]F[−F]F[+F[+F]F[−F]F]F[+F]F[−F]F[−F[+F]F[−F]F]F[+F]F[−F]F]F[+F]F[−F]F[+F[+F]F[−F]F]F[+F]F[−F]F[−F[+F]F[−F]F]F[+F]F[−F]F]F[+F]F[−F]F[+F[+F]F[−F]F]F[+F]F[−F]F[−F[+F]F[−F]F]F[+F]F[−F]F[+F[+F]F[−F]F[+F[+F]F[−F]F]F[+F]F[−F]F[−F[+F]F[−F]F]F[+F]F[−F]F]F[+F]F[−F]F[+F[+F]F[−F]F]F[+F]F[−F]F[−F[+F]F[−F]F]F[+F]F[−F]F[−F[+F]F[−F]F[+F[+F]F[−F]F]F[+F]F[−F]F[−F[+F]F[−F]F]F[+F]F[−F]F]F[+F]F[−F]F[+F[+F]F[−F]F]F[+F]F[−F]F[−F[+F]F[−F]F]F[+F]F[−F]F[−F[+F]F[−F]F[+F[+F]F[−F]F]F[+F]F[−F]F[−F[+F]F[−F]F]F[+F]F[−F]F[+F[+F]F[−F]F[+F[+F]F[−F]F]F[+F]F[−F]F[−F[+F]F[−F]F]F[+F]F[−F]F]F[+F]F[−F]F[+F[+F]F[−F]F]F[+F]F[−F]F[−F[+F]F[−F]F]F[+F]F[−F]F[−F[+F]F[−F]F[+F[+F]F[−F]F]F[+F]F[−F]F[−F[+F]F[−F]F]F[+F]F[−F]F]F[+F]F[−F]F[+F[+F]F[−F]F]F[+F]F[−F]F[−F[+F]F[−F]F]F[+F]F[−F]F]F[+F]F[−F]F[+F[+F]F[−F]F]F[+F]F[−F]F[−F[+F]F[−F]F]F[+F]F[−F]F[+F[+F]F[−F]F[+F[+F]F[−F]F]F[+F]F[−F]F[−F[+F]F[−F]F]F[+F]F[−F]F]F[+F]F[−F]F[+F[+F]F[−F]F]F[+F]F[−F]F[−F[+F]F[−F]F]F[+F]F[−F]F[−F[+F]F[−F]F[+F[+F]F[−F]F]F[+F]F[−F]F[−F[+F]F[−F]F]F[+F]F[−F]F]F[+F]F[−F]F[+F[+F]F[−F]F]F[+F]F[−F]F[−F[+F]F[−F]F]F[+F]F[−F]F'
     },
     {
+        // ANSWER: FF−[−F+F+F]+[+F−F−F]
         axiom: 'F',
         pre: 'F',
         Y: 'FF−[−F+F+F]+[+F−F−F]FF−[−F+F+F]+[+F−F−F]−[−FF−[−F+F+F]+[+F−F−F]+FF−[−F+F+F]+[+F−F−F]+FF−[−F+F+F]+[+F−F−F]]+[+FF−[−F+F+F]+[+F−F−F]−FF−[−F+F+F]+[+F−F−F]−FF−[−F+F+F]+[+F−F−F]]'
     },
     {
+        // ANSWER: A[+A]A
         axiom: 'A',
         pre: 'A',
         Y: 'A[+A]A[+A[+A]A]A[+A]A[+A[+A]A[+A[+A]A]A[+A]A]A[+A]A[+A[+A]A]A[+A]A'
     },
     {
+        // ANSWER: A+[A-A]
         axiom: 'A',
         pre: 'A',
         Y: 'A+[A-A]+[A+[A-A]-A+[A-A]]'
     },
-
 ]
